8a7a029129df71143db727afbee61aef7f317140
RH+ <rhplus0831@proton.me>
2026-07-16 07:53:44 +0900

    fix: expose accordion state and actions

--- src/lib/UI/Accordion.svelte
+++ src/lib/UI/Accordion.svelte
@@ -13,39 +13,62 @@
   }
 
   let { name = '', styled = false, help = '', disabled = false, children, className = '' }: Props = $props()
+  const accordionId = $props.id()
+  const triggerId = `${accordionId}-trigger`
+  const panelId = `${accordionId}-panel`
 </script>
 
 {#if disabled}
   {@render children?.()}
 {:else if styled}
   <div class="flex flex-col mt-2">
-    <button
-      class="hover:bg-selected px-6 py-2 text-lg rounded-t-md border-selected border"
+    <div
+      class="hover:bg-selected rounded-t-md border-selected border flex items-center"
       class:bg-selected={open}
-      class:rounded-b-md={!open}
-      onclick={() => {
-        open = !open
-      }}>
-      <span class="mr-2">{name}</span>
+      class:rounded-b-md={!open}>
+      <button
+        id={triggerId}
+        type="button"
+        aria-expanded={open}
+        aria-controls={panelId}
+        class="min-w-0 grow px-6 py-2 text-left text-lg"
+        onclick={() => {
+          open = !open
+        }}>
+        <span>{name}</span>
+      </button>
       {#if help}
-        <Help key={help} />
-      {/if}</button>
+        <span class="mr-6 shrink-0"><Help key={help} {name} /></span>
+      {/if}
+    </div>
     {#if open}
-      <div class={'flex flex-col border border-selected p-2 rounded-b-md ' + className}>
+      <div
+        id={panelId}
+        role="region"
+        aria-labelledby={triggerId}
+        class={'flex flex-col border border-selected p-2 rounded-b-md ' + className}>
         {@render children?.()}
       </div>
     {/if}
   </div>
 {:else}
   <div class="flex flex-col">
-    <button
-      class="hover:bg-selected px-6 py-2 text-lg"
-      class:bg-selected={open}
-      onclick={() => {
-        open = !open
-      }}>{name}</button>
+    <div class="hover:bg-selected flex items-center" class:bg-selected={open}>
+      <button
+        id={triggerId}
+        type="button"
+        aria-expanded={open}
+        aria-controls={panelId}
+        class="min-w-0 grow px-6 py-2 text-left text-lg"
+        onclick={() => {
+          open = !open
+        }}>{name}</button>
+      {#if help}
+        <span class="mr-6 shrink-0"><Help key={help} {name} /></span>
+      {/if}
+    </div>
     {#if open}
-      <div class="flex flex-col bg-darkbg">
+      <div id={panelId} role="region" aria-labelledby={triggerId} class="flex flex-col bg-darkbg">
         {@render children?.()}
       </div>
     {/if}
